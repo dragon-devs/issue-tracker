1bc4ea61993e5b087b6518c3ef57f1f5bf3bc94f
DRH <dragonfourtyseven@gmail.com>
2024-01-18 03:58:33 +0500

    Improve the user experience

--- app/issues/[id]/edit/DeleteIssueButton.tsx
+++ app/issues/[id]/edit/DeleteIssueButton.tsx
@@ -5,16 +5,20 @@ import {AlertDialog, Button, Flex} from "@radix-ui/themes";
 import {MdDeleteForever} from "react-icons/md";
 import axios from "axios";
 import {useRouter} from "next/navigation";
+import {Spinner} from "@/app/components";
 
 const DeleteIssueButton = ({issueId}: { issueId: number }) => {
-  const [error, setError] = useState(false);
   const router = useRouter();
+  const [error, setError] = useState(false);
+  const [deleting, setDeleting] = useState(false);
   const deleteIssue = async () => {
     try {
+      setDeleting(true)
       await axios.delete('/api/issues/' + issueId);
       router.push('/issues');
       router.refresh()
     } catch (e) {
+      setDeleting(false)
       setError(true)
     }
   }
@@ -22,7 +26,11 @@ const DeleteIssueButton = ({issueId}: { issueId: number }) => {
       <>
         <AlertDialog.Root>
           <AlertDialog.Trigger>
-            <Button color="red"><MdDeleteForever size="18"/>Delete Issue</Button>
+            <Button color="red" disabled={deleting}>
+              <MdDeleteForever size="18"/>
+              Delete Issue
+              {deleting && <Spinner />}
+            </Button>
           </AlertDialog.Trigger>
           <AlertDialog.Content>
             <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
