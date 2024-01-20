d22bb429301e9cd62a3527efc19c39b18c18e876
DRH <dragonfourtyseven@gmail.com>
2024-01-21 00:02:36 +0500

    Fix filtering bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import {Select} from "@radix-ui/themes";
 import {Status} from "@prisma/client";
-import {useRouter} from "next/navigation";
+import {useRouter, useSearchParams} from "next/navigation";
 
 const statuses: { label: string, value?: Status }[] = [
   {label: 'All'},
@@ -13,12 +13,21 @@ const statuses: { label: string, value?: Status }[] = [
 ]
 const IssueStatusFilter = () => {
   const router = useRouter()
+  const searchParams = useSearchParams();
 
   return (
-      <Select.Root onValueChange={(status) => {
-        const query = status === 'default' ? '' : `?status=${status}`
-        router.push('/issues/list' + query);
-      }}>
+      <Select.Root
+          defaultValue={searchParams.get('status') || ''}
+          onValueChange={(status) => {
+            const params = new URLSearchParams();
+
+            if (status) params.append('status', status);
+            if (searchParams.get('orderBy'))
+              params.append('orderBy', searchParams.get('orderBy')!)
+
+            const query = params.size ? '?' + params.toString() : 'default';
+            router.push('/issues/list' + query);
+          }}>
         <Select.Trigger placeholder="Filter by status..."/>
         <Select.Content>
           {statuses.map(status => (
