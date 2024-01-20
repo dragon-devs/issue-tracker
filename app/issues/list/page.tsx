f6863d30ad1f76c0c6fa35bd5fa7b1653bd52aea
DRH <dragonfourtyseven@gmail.com>
2024-01-20 23:45:36 +0500

    Make columns sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,60 +1,91 @@
 import React from 'react';
-import {Table} from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import {Link, IssueStatusBadge} from '@/app/components';
+import { Link, IssueStatusBadge } from '@/app/components';
 import IssueActions from "@/app/issues/list/IssueActions";
-import {Status} from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import NextLink from "next/link";
+import { ArrowUpIcon, ArrowDownIcon } from "@radix-ui/react-icons";
 
 interface Props {
-  searchParams: { status: Status }
+  searchParams: { status: Status; orderBy: keyof Issue; orderDirection?: 'asc' | 'desc' };
 }
 
-const IssuesPage = async ({searchParams}: Props) => {
+const IssuesPage = async ({ searchParams }: Props) => {
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: 'Issue', value: 'title' },
+    { label: 'Status', value: 'status', className: "hidden md:table-cell" },
+    { label: 'Created', value: 'createdAt', className: "hidden md:table-cell" },
+  ];
+
   const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-      ? searchParams.status
-      : undefined;
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
 
   const issues = await prisma.issue.findMany({
-    where: {status}
+    where: { status },
+    orderBy: searchParams.orderBy ? { [searchParams.orderBy]: searchParams.orderDirection || 'asc' } : undefined,
   });
 
   return (
-      <div>
-        <IssueActions/>
-        <Table.Root variant="surface">
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {issues.map(issue => (
-                <Table.Row key={issue.id}>
-                  <Table.Cell>
-                    <Link href={`/issues/${issue.id}`}>
-                      {issue.title}
-                    </Link>
-                    <div className="block md:hidden">
-                      <IssueStatusBadge status={issue.status}/>
-                    </div>
-                  </Table.Cell>
-                  <Table.Cell className="hidden md:table-cell">
-                    <IssueStatusBadge status={issue.status}/>
-                  </Table.Cell>
-                  <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
-                </Table.Row>
+    <div>
+      <IssueActions />
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell key={column.value}>
+
+                <NextLink
+                  href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: column.value,
+                      orderDirection:
+                        column.value === searchParams.orderBy && searchParams.orderDirection === 'asc'
+                          ? 'desc'
+                          : 'asc',
+                    },
+                  }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <>
+                    {searchParams.orderDirection === 'asc' ? (
+                      <ArrowUpIcon className="inline" />
+                    ) : (
+                      <ArrowDownIcon className="inline" />
+                    )}
+                  </>
+                )}
+              </Table.ColumnHeaderCell>
             ))}
-          </Table.Body>
-        </Table.Root>
-      </div>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
   );
 };
 
-// this page is now rendering dynamically.
-export const dynamic = 'force-dynamic'
-
-// export const revalidate = 60; // refresh every 60 seconds
+export const dynamic = 'force-dynamic';
 export default IssuesPage;
