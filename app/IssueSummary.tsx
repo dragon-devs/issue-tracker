0ed117ba73b79568113669df5bcd19f3ef7759c1
DRH <dragonfourtyseven@gmail.com>
2024-01-21 14:39:43 +0500

    Build the IssueSummary.tsx component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Status} from "@prisma/client";
+import {Card, Flex, Text} from "@radix-ui/themes";
+import Link from "next/link";
+
+interface Props {
+  open: number,
+  inProgress: number,
+  closed: number,
+}
+
+const IssueSummary = ({open, inProgress, closed}: Props) => {
+  const containers: {
+    label: string;
+    value: number;
+    status: Status;
+  }[] = [
+    {label: 'Open Issues', value: open, status: 'OPEN'},
+    {label: 'In-progress Issues', value: inProgress, status: 'IN_PROGRESS'},
+    {label: 'Closed Issues', value: closed, status: 'CLOSED'},
+  ]
+  return (
+      <Flex gap="4">
+        {containers.map(container => (
+            <Card key={container.label}>
+              <Flex direction="column" gap="1">
+                <Link className="text-sm font-medium" href={`/issues/list?status=${container.status}`}>
+                  {container.label}
+                </Link>
+              </Flex>
+              <Text size="5" className="font-bold">{container.value}</Text>
+            </Card>
+        ))}
+      </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import {Button, Flex} from "@radix-ui/themes";
 import Pagination from "@/app/components/Pagination";
 import LatestIssues from "@/app/LatestIssues";
+import IssueSummary from "@/app/IssueSummary";
+import prisma from "@/prisma/client";
 
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({
+  where: { status: 'OPEN'}
+  })
+  const inProgress = await prisma.issue.count({
+  where: { status: 'IN_PROGRESS'}
+  })
+  const closed = await prisma.issue.count({
+  where: { status: 'CLOSED'}
+  })
 
   return (
-        <LatestIssues />
+      <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
   )
 }
